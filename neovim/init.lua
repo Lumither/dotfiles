b3ac5cfcbdcd7178cf25c6d029616285b8aeca47
Lumither Tao <lumither@outlook.com>
2025-09-20 15:48:22 -0400

    sync

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,5 +1,10 @@
+vim.cmd('source ~/.vimrc')
+
+require('lazy_setup')
+
 require('config.editor')
 require('config.keymap')
 require('plugins')
 require('config.colorscheme')
 require('config.lsp')
+
